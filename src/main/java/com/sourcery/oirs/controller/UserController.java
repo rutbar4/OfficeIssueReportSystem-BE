5dd06d47d204f95fd9281f0abd29c14aa5076939
Lukrow <lukroza1@gmail.com>
2023-10-12 10:54:27 +0300

    Added User Controller and Service, Endpoint returns mock data

--- src/main/java/com/sourcery/oirs/controller/UserController.java
+++ src/main/java/com/sourcery/oirs/controller/UserController.java
@@ -1,2 +1,21 @@
-package com.sourcery.oirs.controller;public class UserController {
+package com.sourcery.oirs.controller;
+
+import com.sourcery.oirs.model.User;
+import com.sourcery.oirs.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public User getUser() {
+        return userService.getUser();
+    }
 }
--- src/main/java/com/sourcery/oirs/model/User.java
+++ src/main/java/com/sourcery/oirs/model/User.java
@@ -1,2 +1,19 @@
-package com.sourcery.oirs.model;public class User {
+package com.sourcery.oirs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    private String fullName;
+    private String department;
+    private String role;
+    private String address;
+    private String city;
+    private String state;
+    private String postcode;
+    private String country;
 }
