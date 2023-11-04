03e91d5ea50f1566f97e411f35a7e3c1fe09d57d
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2023-11-04 19:15:17 +0200

    feat/SA2FTA-30-edit-issue-details-first edit

--- src/main/java/com/sourcery/oirs/database/repository/IssueRepository.java
+++ src/main/java/com/sourcery/oirs/database/repository/IssueRepository.java
@@ -3,10 +3,7 @@ package com.sourcery.oirs.database.repository;
 
 import com.sourcery.oirs.model.IssueDetailsResponseDto;
 import com.sourcery.oirs.model.Issue;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -42,4 +39,7 @@ public interface IssueRepository {
 
     @Delete("DELETE from issue where id = #{id}")
     void delete(@Param("id") UUID id);
+    @Update("UPDATE issue SET description = #{description}, location = #{location}, status = #{status}, " +
+            "date_modified = #{dateModified} WHERE id = #{id}")
+    void update(IssueDetailsResponseDto issue);
 }
