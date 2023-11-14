7ebdf9f51ce03b5a0718cebedb78eec0b4f1a1d7
SarunasJ-gif <sarunasj82@gmail.com>
2023-11-14 12:41:16 +0200

    Comment Endpoints:

--- src/main/java/com/sourcery/oirs/service/CommentService.java
+++ src/main/java/com/sourcery/oirs/service/CommentService.java
@@ -81,7 +81,7 @@ public class CommentService {
 
     private UUID getCustomUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        log.info("User from context in Commnet Service: " + authentication.getName());
+        log.info("User from context in Comment Service: " + authentication.getName());
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
         return customUserDetails.getId();
     }
