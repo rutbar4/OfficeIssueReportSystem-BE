ca02f86cc9a565eefa37405b33285bea86dc953c
Justinas Teselis <teselisjustinas@gmail.com>
2023-11-14 14:36:01 +0200

    SA2FTA-101 Testing with an added slash

--- src/main/java/com/sourcery/oirs/CorsConfig.java
+++ src/main/java/com/sourcery/oirs/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://asdf.devbstaging.com")
+                .allowedOrigins("http://localhost:3000", "https://asdf.devbstaging.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
