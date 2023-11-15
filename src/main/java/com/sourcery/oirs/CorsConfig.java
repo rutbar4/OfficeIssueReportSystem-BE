d580718d6f9c2d65b4a59cf5e5133ba0d9350940
Justinas Teselis <teselisjustinas@gmail.com>
2023-11-16 00:19:20 +0200

    SA2FTA-101 Testing without EnableWebMvc

--- src/main/java/com/sourcery/oirs/CorsConfig.java
+++ src/main/java/com/sourcery/oirs/CorsConfig.java
@@ -6,14 +6,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://asdf.devbstaging.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                //.allowCredentials(true)
+                .allowCredentials(true)
                 .allowedHeaders("*");
     }
 }
