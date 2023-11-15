85319c9adc28b77487d531fd46da9da4721e5bc1
SarunasJ-gif <sarunasj82@gmail.com>
2023-11-15 20:52:53 +0200

    SA2FTA-14: deleted endpoint for testing

--- src/main/java/com/sourcery/oirs/controller/UserController.java
+++ src/main/java/com/sourcery/oirs/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.sourcery.oirs.controller;
 
 import com.sourcery.oirs.model.User;
-import com.sourcery.oirs.security.CustomUserDetails;
 import com.sourcery.oirs.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +18,8 @@ import java.util.UUID;
 public class UserController {
     private final UserService userService;
 
-
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserById(@PathVariable UUID id) {
         return userService.getUserById(id);
-    }
-
-
-    @GetMapping("/from-context")
-    public UUID getUserFromContext() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        UUID userId = customUserDetails.getId();
-        return userId;
     }
 }
